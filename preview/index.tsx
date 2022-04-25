27fdedab58cb069ec875ca426e6031af8273a60d
limichange <limichange@hotmail.com>
2022-04-25 10:58:20 +0800

    feat: setting preview

---
+++ preview/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import ReactDOM from 'react-dom'
+
+import { Marquee } from '../src'
+
+// create a React app
+ReactDOM.render(<Marquee>Hello, world!</Marquee>, document.getElementById('app'))
